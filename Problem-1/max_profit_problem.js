07161ddb22218b32ff14e1796574e4caff89a894
akash-ck7 <vidhyasagar.s161199@gmail.com>
2025-05-25 17:16:57 +0530

    Initial commit of IMDB Clone

---
+++ Problem-1/max_profit_problem.js
@@ -0,0 +1,56 @@
+function calculateBestEarnings(totalTime) {
+  let highestProfit = 0;
+  let selectedPlan = { theatres: 0, pubs: 0, parks: 0 };
+
+  // Trying every possible number of commercial parks we can build
+  for (let parks = 0; parks <= Math.floor(totalTime / 10); parks++) {
+    let tLeftAfterPark = totalTime - parks * 10;
+
+    // Trying every possible number of theatres we can build after parks
+    for (
+      let theatres = 0;
+      theatres <= Math.floor(tLeftAfterPark / 5);
+      theatres++
+    ) {
+      let usedbytheatre = theatres * 5;
+      let tLeftAfterTheatre = tLeftAfterPark - usedbytheatre;
+      // Trying every number of pub we can squeeze in the remaining period of time
+
+      for (let pubs = 0; pubs <= Math.floor(tLeftAfterTheatre / 4); pubs++) {
+        let totalTimeUsed = parks * 10 + theatres * 5 + pubs * 4;
+        if (totalTimeUsed > totalTime) {
+          continue;
+        }
+        // Each building will earns money every unit of time * after* it's built
+        let parkIncome = parks * 3000 * (totalTime - parks * 10);
+        let theatreIncome =
+          theatres * 1500 * (totalTime - parks * 10 - theatres * 5);
+        let pubIncome =
+          pubs * 1000 * (totalTime - parks * 10 - theatres * 5 - pubs * 4);
+
+        let totalIncome = parkIncome + theatreIncome + pubIncome;
+
+        // we need to save the best combination
+        if (totalIncome > highestProfit) {
+          highestProfit = totalIncome;
+          selectedPlan = {
+            theatres: theatres,
+            pubs: pubs,
+            parks: parks,
+          };
+        }
+      }
+    }
+  }
+  // Printing the output  in a readable format
+  console.log(`Time Unit: ${totalTime}`);
+  console.log(`Earnings: $${highestProfit}`);
+  console.log(
+    `Solution: T: ${selectedPlan.theatres}, P: ${selectedPlan.pubs}, C: ${selectedPlan.parks}`
+  );
+}
+
+// for few example cases
+calculateBestEarnings(7); // it Should be $3000
+calculateBestEarnings(8); // it Should be $4500
+calculateBestEarnings(13); //it  Should be $16500
---
+++ README.md
@@ -0,0 +1,13 @@
+I solved both the problems
+
+You can clone this into your local and run below comand for run my code
+
+For Problem 1 :
+run
+node max_profit_problem.js
+
+you will see the output in console
+
+For Problem 2:
+run
+index.html file you will see the output in the browser
