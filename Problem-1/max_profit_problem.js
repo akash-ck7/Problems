98d84a7bd756cc6d29ced076acf0ad8af035cdfd
akash-ck7 <vidhyasagar.s161199@gmail.com>
2025-05-29 11:51:20 +0530

    changes

--- Problem-1/max_profit_problem.js
+++ Problem-1/max_profit_problem.js
@@ -1,5 +1,3 @@
-// Recursive DFS with backtracking approach
-
 function getmaxprofit(n) {
   const buildings = [
     { name: "T", time: 5, rate: 1500 },
@@ -8,21 +6,22 @@ function getmaxprofit(n) {
   ];
 
   let maxProfit = 0;
-  let bestCombo = { T: 0, P: 0, C: 0 };
-  // passing the inputs
+  let bestCombos = [];
+
   function dfs(currentTime, profit, combo) {
     let updated = false;
 
     for (let b of buildings) {
-      if (currentTime + b.time <= n) {
-        const nextTime = currentTime + b.time;
-        const operationalTime = n - nextTime;
+      const buildEndTime = currentTime + b.time;
+      const operationalTime = n - buildEndTime;
+      // ensures that only buildings with positive profit potentieal are to be added
+      if (buildEndTime <= n && operationalTime > 0) {
         const gain = b.rate * operationalTime;
 
         const newCombo = { ...combo };
         newCombo[b.name] = (newCombo[b.name] || 0) + 1;
 
-        dfs(nextTime, profit + gain, newCombo);
+        dfs(buildEndTime, profit + gain, newCombo);
         updated = true;
       }
     }
@@ -30,20 +29,27 @@ function getmaxprofit(n) {
     if (!updated) {
       if (profit > maxProfit) {
         maxProfit = profit;
-        bestCombo = combo;
+        bestCombos = [combo];
+      } else if (profit === maxProfit) {
+        bestCombos.push(combo);
       }
     }
   }
 
-  dfs(0, 0, { T: 0, P: 0, C: 0 });
-  //return the combos here
+  dfs(0, 0, {});
+
+  //formating the comobos
+  const formattedCombos = bestCombos.map((combo) => {
+    return `T: ${combo.T || 0}, P: ${combo.P || 0}, C: ${combo.C || 0}`;
+  });
+
   return {
-    combo: `T: ${bestCombo.T} P: ${bestCombo.P} C: ${bestCombo.C}`,
-    profit: `$${maxProfit}`,
+    maxEarnings: maxProfit,
+    combinations: formattedCombos,
   };
 }
 
-// Test Cases for all three metioned in the documents
-console.log("time = 7 :", getmaxprofit(7)); // ans:$3000
-console.log("time = 8 :", getmaxprofit(8)); // ans: $4500
-console.log("time = 13 :", getmaxprofit(13)); // ans: $16500
+// for testing
+console.log("Time = 49:", getmaxprofit(49));
+console.log("Time = 8:", getmaxprofit(8));
+console.log("Time = 13:", getmaxprofit(13));
